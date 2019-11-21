70c815ef6179b4968467f4ab16533278da361485
abhishekpattnaik <abhishekpattnaik06@gmail.com>
2019-11-21 12:24:27 +0530

    Commit for Cricket

--- Cricket.py
+++ Cricket.py
@@ -1,4 +1,28 @@
-players = {
+players = [
+        {
+                "Rohit":{"Run" : 200,
+                        "Wickets" : 10,
+                        "Average runs" : 43},
+                "Dhoni":{"Run" : 200,
+                        "Wickets" : 10,
+                        "Average runs" : 43},
+                "Shikhar":{"Run" : 200,
+                        "Wickets" : 10,
+                        "Average runs" : 43},
+                'Gambhir':{"Run" : 200,
+                        "Wickets" : 10,
+                        "Average runs" : 43},
+                'Shami':{"Run" : 200,
+                        "Wickets" : 10,
+                        "Average runs" : 43},
+                'Kohli':{"Run" : 200,
+                        "Wickets" : 10,
+                        "Average runs" : 43},
+                'Abhishek':{"Run" : 200,
+                        "Wickets" : 10,
+                        "Average runs" : 43}
+        },
+        {
         "Rohit":{"Run" : 200,
                 "Wickets" : 10,
                 "Average runs" : 43},
@@ -20,9 +44,85 @@ players = {
         'Abhishek':{"Run" : 200,
                 "Wickets" : 10,
                 "Average runs" : 43}
-}
+        },
+        {
+        "Rohit":{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        "Dhoni":{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        "Shikhar":{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Gambhir':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Shami':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Kohli':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Abhishek':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43}
+        },
+        {
+        "Rohit":{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        "Dhoni":{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        "Shikhar":{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Gambhir':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Shami':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Kohli':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Abhishek':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43}
+        },
+        {
+        "Rohit":{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        "Dhoni":{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        "Shikhar":{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Gambhir':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Shami':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Kohli':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43},
+        'Abhishek':{"Run" : 200,
+                "Wickets" : 10,
+                "Average runs" : 43}
+        }
+]
 lis=[]
-for i in players.values():
+'''for i in players.values():
         for j in i.values():
                 lis.append(j)
 print(lis)
+'''
+for i in players:
+        for j in i.values():
+                for k in j.values():
+                        lis.append(k)
+print(k)
