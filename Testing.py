1055cc41d9ba419abb0062e150ba80b77e7d3fbc
abhishekpattnaik <abhishekpattnaik06@gmail.com>
2019-11-28 15:04:08 +0530

    Nov 28 15:04

--- Testing.py
+++ Testing.py
@@ -1,14 +1,22 @@
 import pandas as pd 
+# import pdb;pdb.set_trace() 
 x1 = pd.read_excel('trafficking.xlsx')
 c = 0
 D = {}
 for i in x1.advertiser_name:
     x2=x1.loc[c]
     c=c+1
-    if i in D.values():
-        D[i][x2.brand_name]=D[i][x2.brand_name].append([x2.show])
+    if i in D.keys():
+        if x2.brand_name in D[i].keys():
+            D[i][x2.brand_name].append(x2.show)
+        else:
+            D[i][x2.brand_name]=[x2.show]
     else:
         D[i]={x2.brand_name:[x2.show]}
 x3 = pd.DataFrame(D)
 x3.to_csv('testing1.csv')
-print('End')
+for i in D.keys():
+    for j in D[i].keys():
+        for k in D[i][j]:
+            print(i,'(Advertiser Name)\t',j,'(Brand Name)\t',k,'(Shows)')
+print('Program ends here \n thank you')
--- Testing1.py
+++ Testing1.py
@@ -1,5 +1,6 @@
 import pandas as pd 
 import pprint 
+import yaml
 x1 = pd.read_excel('trafficking.xlsx')
 c = 0
 D = {}
@@ -13,5 +14,15 @@ for i in x1.advertiser_name:
             D[i][x2.brand_name]=[x2.show]
     else:
         D[i]={x2.brand_name:[x2.show]}
+# for i in D.keys():
+#     print('{',i,':')
+#     for j in D[i].keys():
+#         print('\t{',j,':[',end='')
+#         for k in D[i][j]:
+#             print(k,',',end='')
+#         print(']}')
+#     print('}')
+# print('Program ends here \n thank you')
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(D)
+# print(yaml.dump(D, default_flow_style=False))
